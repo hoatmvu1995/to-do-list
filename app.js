72ddbd402c38003635dcf77dd6a1e3d6839a6594
vuh <vuh@ecs.csus.edu>
2020-10-08 10:00:16 -0600

    create a simple todo list app and initialize it to github

---
+++ app.js
@@ -0,0 +1,39 @@
+//Selector
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listener
+todoButton.addEventListener('click', addTodo);
+
+//Function 
+function addTodo() {
+    event.preventDefault();
+    //todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo")
+
+    //create li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //check trash button 
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //append to List
+    todoList.appendChild(todoDiv);
+
+    //clear todo input value 
+    todoInput.value ="";
+}
