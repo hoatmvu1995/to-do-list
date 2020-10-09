1e93b10e4b64ddddf86a68e7f4415fe1446a65b0
vuh <vuh@ecs.csus.edu>
2020-10-09 00:02:21 -0600

    1. enable delete and completed todo list, 2. Categorize todo list items. 3. Fixing bug with uncompleted items which unable to display uncompleted items.

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
-
+const filterOption = document.querySelector('.filter-todo');
 //Event Listener
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Function 
 function addTodo() {
@@ -36,4 +38,56 @@ function addTodo() {
 
     //clear todo input value 
     todoInput.value ="";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    //Delete TODO
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        })
+    }
+
+    //check mark
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+
+    todos.forEach((todo) => {
+        
+            switch(e.target.value) {
+                case "all":
+                    todo.style.display = 'flex';
+                    break;
+
+                case "completed":
+                    if ( todo.classList.contains("completed") ) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+                    
+                    break;
+
+                case "uncompleted":
+                    if ( todo.classList.contains("uncompleted") ) {
+                        todo.style.display = 'flex';
+                    } else {
+                        todo.style.display = 'none';
+                    }
+
+                    break;        
+            }
+    }
+    );
 }
